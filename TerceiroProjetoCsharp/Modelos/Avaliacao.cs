23d1f1c03e66ad57545a536cb3d6636e5a10372c
Andres Neto <andres.fgn@gmail.com>
2023-08-31 16:43:18 -0300

    Alterações parte 3

--- TerceiroProjetoCsharp/Modelos/Avaliacao.cs
+++ TerceiroProjetoCsharp/Modelos/Avaliacao.cs
@@ -4,14 +4,16 @@ internal class Avaliacao
 {
     public Avaliacao(int nota)
     {
-        Nota = nota;
+        if (nota < 0) nota = 0;
+        if (nota > 10) nota = 10;
+        Nota = nota;        
     }
 
     public int Nota { get; }
 
     public static Avaliacao Parse(string texto)
-    {
-        int nota = int.Parse(texto);
+    {  
+        int nota = (int)double.Parse(texto);
         return new Avaliacao(nota);
     }
 }
